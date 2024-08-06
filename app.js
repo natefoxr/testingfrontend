78e9aba413859fcb4f5960f310cec0bd868adf3f
Nate Moudakis <105576226+natefoxr@users.noreply.github.com>
2024-08-06 01:51:08 -0700

    Update app.js

--- app.js
+++ app.js
@@ -5,17 +5,17 @@ const targetId = document.getElementById('targetId');
         
 triggerGong.addEventListener('click', () => {
         // Trigger the supergong
-        targetId.innterHTML = 150000;
+        targetId.innterHTML = "150000";
         setTimeout(function(){
                 console.log("Sending Super Gong Trigger")
         }, 2000);
         
         uid = +uniqueId;
-        uniqueId.innerHTML = uid + 1;
-        targetId.innterHTML = 500000;
+        uniqueId.innerHTML = str(uid + 1);
+        targetId.innterHTML = "500000";
         setTimeout(function(){
                 console.log("Reseting Super Gong Trigger")
-                targetId.innterHTML = 150000;
+                targetId.innterHTML = "150000";
         }, 2000);
 });
 
